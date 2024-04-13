e4db841d8355c9661685956c8c565b57cb7c1522
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-04-13 09:51:24 +0900

    migrateリセットをなくして元に戻した

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ rm -f /app/tmp/pids/server.pid
 
 RAILS_ENV=production bundle exec rails assets:precompile
 RAILS_ENV=production bundle exec rails assets:clean
-# RAILS_ENV=production bundle exec rails db:migrate
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+RAILS_ENV=production bundle exec rails db:migrate
+
 
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
 exec "$@"
